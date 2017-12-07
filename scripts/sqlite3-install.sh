1ca79bb77d05b9192a7c0a30c35e71e077143d32
Karl Lew <karl@firepick.org>
2017-12-07 04:35:52 -0800

    sql

--- scripts/sqlite3-install.sh
+++ scripts/sqlite3-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo -e "START\t: $0 `date`"
-pushd `dirname $0` >& /dev/null
+#pushd `dirname $0` >& /dev/null
+SCRIPTS=`dirname $0`
+echo -e "INSTALL\t: `pwd`"
 
 SQLVER=`sqlite3 -version`
 RC=$?
@@ -21,7 +23,7 @@ if [ -e oyamist.db ]; then
     echo -e "INSTALL\t: SQLite database exists:oyamist.db"
 else
     echo -e "INSTALL\t: Creating SQLite database: oyamist.db"
-    sqlite3 ../oyamist.db < create.sql
+    sqlite3 oyamist.db < ${SCRIPTS}/create.sql
 fi
 
 echo -e "END\t: $0 `date`"
