cedc40846ebb5c2ebf64e110108a72fa35b5a3fa
Karl Lew <karl@firepick.org>
2017-12-25 09:46:33 -0800

    chart

---
+++ src/ui/oya-chart-panel.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div>
+        <oya-chart palette="red" sensorProp="tempInternal" service="test"></oya-chart>
+        <oya-chart palette="blue" sensorProp="humidityInternal" service="test"></oya-chart>
+        <oya-chart palette="red" sensorProp="tempExternal" service="test"></oya-chart>
+        <oya-chart palette="blue" sensorProp="humidityExternal" service="test"></oya-chart>
+        <oya-chart palette="red" sensorProp="tempAmbient" service="test"></oya-chart>
+        <oya-chart palette="blue" sensorProp="humidityAmbient" service="test"></oya-chart>
+    </div>
+
+</template><script>
+
+export default {
+    name: 'oya-chart-panel',
+}
+
+</script><style>
+
+</style>
