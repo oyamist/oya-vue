77b3fbd41edbc4df020cc40e006b51cb844d5ab9
Karl Lew <karl@firepick.org>
2018-02-03 20:51:01 -0800

    cal

--- src/ui/oya-chart-panel.vue
+++ src/ui/oya-chart-panel.vue
@@ -7,7 +7,7 @@
                  :nudge-right="40" max-width="290px" min-width="290px" >
                 <v-text-field slot="activator" label="Chart End Date" readonly
                     @blur="date = parseDate(dateFormatted)"
-                    v-model="dateFormatted" prepend-icon="event" >
+                    v-model="dateFormatted" >
                 </v-text-field>
                 <v-date-picker v-model="date" scrollable actions
                     @input="dateFormatted = formatDate($event)">
@@ -20,7 +20,19 @@
                   </template>
                 </v-date-picker>
             </v-menu>
-            <v-btn icon @click="clickSettings"><v-icon>settings</v-icon></v-btn>
+            <div>
+                <v-menu offset-y>
+                  <v-btn slot=activator icon><v-icon>settings</v-icon></v-btn>
+                  <v-list>
+                    <v-list-tile @click="clickSettings">
+                      <v-list-tile-title>Edit Chart Settings</v-list-tile-title>
+                    </v-list-tile>
+                    <v-list-tile @click="calDialog=true">
+                      <v-list-tile-title>Calibrate nutrient sensor</v-list-tile-title>
+                    </v-list-tile>
+                  </v-list>
+                </v-menu>
+            </div>
         </div>
         <div v-if="this.rbService['oya-conf'].apiModel">
             <oya-chart palette="red" sensorProp="tempInternal" :date="date" 
@@ -42,6 +54,93 @@
             <oya-chart palette="blue" sensorProp="ecAmbient" :date="date" 
                 :stepSize="stepSize('ec')" :service='service'></oya-chart>
         </div>
+        <v-dialog v-if="ecSensor" v-model="calDialog" max-width="90%" persistent>
+            <v-card class="pl-3 pr-3">
+                <v-card-title class="title">Calibrate nutrient sensor {{ecSensor.name}}</v-card-title>
+                <v-card-text>
+                    Nutrient recipe affects conductivity measurement.
+                    <v-text-field type="text" v-model="calText"
+                        placeholder='Enter description (e.g., "GH Flora Grow/Micro/Bloom 2:2:2")'
+                        class="input-group" />
+                </v-card-text>
+                <v-card-text>
+                    Choose date with large temperature variations for training nutrient sensor AI.
+                    <v-menu lazy :close-on-content-click="false" v-model="ecmenu"
+                         transition="scale-transition" 
+                         :nudge-right="40" max-width="290px" min-width="290px" >
+                        <v-text-field slot="activator" readonly v-model="calDate"
+                            @blur="calPickerDate = toPickerDate(calDate)"
+                        ></v-text-field>
+                        <v-date-picker v-model="calPickerDate" scrollable actions
+                            @input="calDate = fromPickerDate($event)">
+                          <template slot-scope="{ save, cancel }">
+                            <v-card-actions>
+                              <v-spacer></v-spacer>
+                              <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
+                              <v-btn flat color="primary" @click="save">OK</v-btn>
+                            </v-card-actions>
+                          </template>
+                        </v-date-picker>
+                    </v-menu>
+                </v-card-text>
+                <v-card-text>
+                    Choose measurement unit.
+                    <v-select 
+                        v-bind:items="nutrientUnits()"
+                        v-model="ecUnit"
+                    ></v-select>
+                    <div v-if="ecUnit!=='%'">
+                        <p> Enter nominal measurement value to display for calibration solution
+                        </p>
+                        <v-text-field type="number" v-model="ecNominal"
+                            label='Nominal calibration value' class="input-group" />
+                    </div>
+                </v-card-text>
+                <v-alert type=error v-show="alertCalError">
+                    <div v-show="alertCalError"> {{alertCalError}} </div>
+                </v-alert>
+                <v-card-actions>
+                    <v-spacer/>
+                    <div v-show="alertCalWait">{{alertCalWait}}</div>
+                    <v-btn color="grey lighten-2" @click="calDialog=false">Cancel</v-btn>
+                    <v-btn color="primary" @click="calibrate('ecInternal')">Calibrate</v-btn>
+                </v-card-actions>
+                <v-card-text>
+                    <a @click="calDialogMore=!calDialogMore">more...</a>
+                </v-card-text>
+                <v-card-text v-show="calDialogMore">
+                    <h3>Why calibrate?</h3>
+                    Nutrient conductivity measurement is affected
+                    by temperature, time, and nutrient recipes.
+                    Calibration provides temperature independent measurement.
+
+                    <h3>What calibration solution should I use?</h3>
+                    For convenience, calibrate sensor using actual nutrient solution.
+                    Use a reference solution for calibration if nutrient recipes will
+                    change during crop cycle.
+
+                    <h3>How much calibration solution do I need?</h3>
+                    Use 1 cup or 250ml of calibration solution to ensure largest temperature variation.
+                    Larger amounts of calibration solution will eliminate the temperature variations
+                    required for optimal calibration.
+
+                    <h3>What probes do I need?</h3>
+                    Immerse the following probes together directly into the calibration solution:
+                    <div class="pl-4">
+                        <ul>
+                            <li>Temperature sensor (location: Internal)</li>
+                            <li>Conductivity sensor such as Atlas Scientific EZO EC K1. (location: Internal)</li>
+                        </ul>
+                    </div>
+
+                    <h3>What about 1-point and 2-point calibration?</h3>
+                    Linear 1-point and 2-point calibrations are inaccurate. They are inaccurate because
+                    the relationship between nutrient conductivity and temperature is non-linear.
+                    The OyaMist sensor AI uses a day of training data for calibration.
+
+                </v-card-text>
+            </v-card>
+        </v-dialog>
         <rb-api-dialog :apiSvc="apiSvc" v-if="apiModelCopy && apiModelCopy.rbHash">
             <div slot="title">Chart Settings</div>
             <v-card flat>
@@ -56,43 +155,6 @@
                         <v-checkbox label="Display raw sensor values" 
                             v-model="apiModelCopy.chart.showRaw" light>
                         </v-checkbox>
-                    </rb-dialog-row>
-                    <rb-dialog-row label="Nutrients">
-                        <v-select 
-                            v-bind:items="nutrientUnits()"
-                            v-model="apiModelCopy.chart.ecUnits"
-                            label="Units"
-                            ></v-select>
-                        <v-text-field type="number" v-model="apiModelCopy.chart.ecStepSize"
-                            label="Calibration solution value" class="input-group" />
-                        <v-text-field type="text" v-model="calText"
-                            placeholder="Enter solution name"
-                            label="Calibration solution" class="input-group" />
-                        <v-menu lazy :close-on-content-click="false" v-model="ecmenu"
-                             transition="scale-transition" offset-y  
-                             :nudge-right="40" max-width="290px" min-width="290px" >
-                            <v-text-field slot="activator" readonly v-model="calDate"
-                                label="Calibration start date" prepend-icon="event" 
-                                @blur="calPickerDate = toPickerDate(calDate)"
-                            ></v-text-field>
-                            <v-date-picker v-model="calPickerDate" scrollable actions
-                                @input="calDate = fromPickerDate($event)">
-                              <template slot-scope="{ save, cancel }">
-                                <v-card-actions>
-                                  <v-spacer></v-spacer>
-                                  <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
-                                  <v-btn flat color="primary" @click="save">OK</v-btn>
-                                </v-card-actions>
-                              </template>
-                            </v-date-picker>
-                        </v-menu>
-                        <v-btn color="primary" @click="calibrate('ecInternal')">Calibrate</v-btn>
-                        <v-alert type=warning v-show="alertCalWait" color="orange">
-                            {{alertCalWait}}
-                        </v-alert>
-                        <v-alert type=error v-show="alertCalError">
-                            <div v-show="alertCalError"> {{alertCalError}} </div>
-                        </v-alert>
                     </rb-dialog-row>
                 </v-card-text>
             </v-card>
@@ -117,6 +179,8 @@ export default {
         var date = calPickerDate;
         var dateFormatted = this.formatDate(date);
         return {
+            calDialog: false,
+            calDialogMore: false,
             calText: null,
             alertCalWait: null,
             alertCalError: null,
@@ -124,29 +188,40 @@ export default {
             calPickerDate,
             date,
             dateFormatted,
+            ecUnit: '%',
+            ecNominal: 100,
             reportDateMenu: false,
             ecmenu: false,
         }
     },
     props: {
     },
+    computed: {
+        ecSensor() {
+            var oyaConf = this.rbService['oya-conf'];
+            var apiModel = oyaConf && oyaConf.apiModel;
+            var sensors = apiModel && apiModel.sensors;
+            return sensors && sensors.filter(s=>s.readEC)[0];
+        }
+    },
     methods: {
         calibrate(field) {
-            this.alertCalWait = "Calibration in progress...";
+            this.alertCalWait = "Calibrating...";
             var opts = {
                 startDate: this.calPickerDate,
-                unit: '%',
-                name: this.calText,
+                nominal: this.ecNominal,
+                unit: this.ecUnit,
+                name: this.calText || "(unknown solution)",
             };
             console.log('calibrate', opts);
             var url = [this.restOrigin(), this.service, 'sensor', 'calibrate'].join('/');
             this.$http.post(url, opts).then(r => {
                 this.alertCalWait = null;
                 var cal = r.data;
-                var msg = `Calibration complete: ${cal.name} value:${cal.nominal} ${cal.unit}`; 
+                var msg = `Calibrated ${cal.name} = ${cal.nominal} ${cal.unit}`; 
                 console.log(msg);
                 this.alertSuccess(msg);
-                this.apiCancel();
+                this.calDialog = false;
             }).catch(e => {
                 this.alertCalWait = null;
                 this.alertCalError = e;
@@ -167,7 +242,7 @@ export default {
         },
         nutrientUnits() {
             return [{
-                text: "% of calibration solution",
+                text: "Nutrients available (100%)",
                 value: "%",
             },{
                 text: "parts/million (ppm)",
@@ -213,6 +288,10 @@ export default {
         var reportDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
         console.log('now', now, now.getDate());
         Vue.set(this.rbService, 'reportDate', reportDate);
+        this.onApiModelLoaded(apiModel => {
+            console.log('ecSensor', ecSensor);
+            this.calText = this.ecSensor.tempCal.name;
+        });
     },
 }
 </script><style>
